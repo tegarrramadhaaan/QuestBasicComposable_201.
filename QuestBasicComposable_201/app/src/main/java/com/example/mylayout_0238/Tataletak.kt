1d9e636a23bb748ac90ad09a6e2745eb7a6c622e
tegarrramadhaaan <anakrama2000@gmail.com>
2025-10-16 16:38:02 +0700

    buat layout Box sederhana dengan beberapa Text

--- QuestBasicComposable_201/app/src/main/java/com/example/mylayout_0238/Tataletak.kt
+++ QuestBasicComposable_201/app/src/main/java/com/example/mylayout_0238/Tataletak.kt
@@ -41,4 +41,19 @@ fun TataletakRow(modifier: Modifier) {
     }
 }
 
+@Composable
+fun TataletakBox(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxHeight()
+            .fillMaxWidth(), contentAlignment = Alignment.Center
+    ){
+        Text(text = "Box 1")
+        Text(text = "Column 1")
+        Text(text = "Row 1")
+        Text(text = "Box 2")
+        Text(text = "Column 2")
+    }
+}
+
 
