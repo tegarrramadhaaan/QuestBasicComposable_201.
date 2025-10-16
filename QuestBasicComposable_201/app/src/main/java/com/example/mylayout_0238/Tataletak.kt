6a64c4cf755408bd948e78de9429198e6bf85e5c
tegarrramadhaaan <anakrama2000@gmail.com>
2025-10-16 16:35:59 +0700

    tambahkan fungsi TataletakRow untuk layout horizontal

--- QuestBasicComposable_201/app/src/main/java/com/example/mylayout_0238/Tataletak.kt
+++ QuestBasicComposable_201/app/src/main/java/com/example/mylayout_0238/Tataletak.kt
@@ -30,4 +30,15 @@ fun TataletakColumn(modifier: Modifier) {
     }
 }
 
+@Composable
+fun TataletakRow(modifier: Modifier) {
+    Row(modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}
+
 
